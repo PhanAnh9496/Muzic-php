e9fe02383872d892dc362c9c1fe003bd50305fd7
unknown <phananh9496@gmail.com>
2019-09-18 16:01:49 +0700

    update 18/09/2019

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@ include("includes/classes/User.php");
 include("includes/classes/Artist.php");
 include("includes/classes/Album.php");
 include("includes/classes/Song.php");
-// include("includes/classes/Playlist.php");
+include("includes/classes/Playlist.php");
 
 //session_destroy(); LOGOUT
 
